de0ba4340167e8f569189e29ce6d0b61c0a4417b
gabrielferreira23 <gabriel_ferreira23@estudante.sesisenai.org.br>
2025-11-25 09:27:52 -0300

    trem

--- trem/trem.ino
+++ trem/trem.ino
@@ -1,49 +1,63 @@
+// Bibliotecas e Includes
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <WiFiClientSecure.h>
 #include "env.h"
 
+// Inicialização de objetos
 WiFiClientSecure espClient;
 PubSubClient mqttClient(espClient);
 
-
-const String brokerUser = "";
-const String BrokerPass = "";
-
+// Pinos
 const int ledPin = 2;
 
+// Setup
 void setup() {
-  Serial.begin(115200);    //configura a placa pra mostrar na tela
-  wifiClient.setInsecure();  // Desabilita a verificação de certificado
-  WiFi.begin(WIFI_SSID, WIFI_PASS);  //tenta conectar na rede
+
+// Conexão WiFi
+  Serial.begin(115200);    // inicia a Serial para ver mensagens de debug
+  espClient.setInsecure();  // desativa verificação do certificado (somente para testes)
+  WiFi.begin(WIFI_SSID, WIFI_PASS);  // inicia conexão WiFi
   Serial.println("Conectando no WiFi");
-  Serial.println("Conectado com sucesso");
-  mqttClient.setServer(brokerURL.c_str(), brokerPort);
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(500);
+  Serial.print(".");
+  }
+  Serial.println("\nConectado com sucesso!");
+
+// Conexão MQTT
+  mqttClient.setServer(BROKER_URL, BROKER_PORT);
   String userID = "trem";
   userID += String(random(0xffff), HEX);
   while (mqttClient.connect(userID.c_str()) == 0) {
     Serial.print(".");
     delay(200);
   }
-  mqttClient.subscribe(topicPresence1.c_str());
-  mqttClient.setCallback(callback);  // Função ao receber mensagem no tópico
+
+// Inscrição e Callback
+  mqttClient.setCallback(callback); // define a função chamada quando chegar mensagem no tópico
+  mqttClient.subscribe(TOPIC_PONTEH); // inscreve no tópico configurado
   Serial.println("\n Conectado com sucesso!");
+
+// Configuração de pinos
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
 }
 
+// Loop principal
 void loop() {
   String mensagem = "";  // lê letra por letra e adiciona \n automático no final
   if (Serial.available() > 0) {
     mensagem = Serial.readStringUntil('\n');
     mensagem = ": " + mensagem;
-    mqttClient.publish("", mensagem.c_str());  // Envia a mensagem pro servidor Tópico
+    mqttClient.publish(TOPIC_STATUS, mensagem.c_str());  // Envia a mensagem pro servidor Tópico
     Serial.println(mensagem);
   }
   mqttClient.loop();
 }
 
-void callback(char* topicPresence1, byte* payload, unsigned long length) {  // Tópico, mensagem em si, tamanho da mensagem
+// Função Callback - chamada ao receber mensagem MQTT
+void callback(char* topic, byte* payload, unsigned int length) {  // Tópico, mensagem em si, tamanho da mensagem
   String MensagemRecebida = "";
   for (int i = 0; i < length; i++) {
     // Pega cada letra de payload e junta na mensagem
@@ -51,7 +65,7 @@ void callback(char* topicPresence1, byte* payload, unsigned long length) {  // T
   }
   Serial.println(MensagemRecebida);
 
-  // Código para acender ou apagar o led (pino 2)
+// Código para acender ou apagar o led (pino 2)
   digitalWrite(2, HIGH);  // Pisca ao receber mensagem
   delay(1000);            
   digitalWrite(2, LOW);
